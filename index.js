244d88c787a9b16ae561db56d7aa9c5c17021d18
Nathan Harper <iex-eng+nathanharper@googlegroups.com>
2015-10-07 14:41:40 -0400

    check git status

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+'use strict';
 var fs = require('fs');
 var path = require('path');
 var join = path.join;
+var exec = require('child_process').exec;
 
 function Plugin(files) {
   this.context = path.dirname(module.parent.filename);
@@ -11,27 +13,33 @@ function Plugin(files) {
   } else {
     this.files = files || [];
   }
-
 }
 
 // hook into webpack
 Plugin.prototype.apply = function(compiler) {
   var self = this;
-  return compiler.plugin('done', function() {
+  return compiler.plugin('done', function(stats) {
     self.files.forEach(function(file){
-      var file = join(self.context, file);
-      var json = self.increment(file);
-      fs.writeFile(file, JSON.stringify(json, null, 2));
+      var fullFile = join(self.context, file);
+      exec('cd ' + self.context + '; git show HEAD:' + file, function (error, stdout, stderr) {
+        var json = self.increment(fullFile, stats.hash, JSON.parse(stdout));
+        fs.writeFile(fullFile, JSON.stringify(json, null, 2));
+      });
     });
   });
 }
 
 // increment build number
-Plugin.prototype.increment = function(file) {
+Plugin.prototype.increment = function(file, hash, oldJSON) {
   var json = require(file);
   var versions = json.version.split('.');
-  versions[2] = parseInt(versions[2]) + 1;
+
+  if (json.version === oldJSON.version && hash !== oldJSON.webpackAssetHash) {
+    versions[2] = parseInt(versions[2]) + 1;
+  }
+
   json.version = versions.join('.');
+  json.webpackAssetHash = hash;
   return json;
 }
 
